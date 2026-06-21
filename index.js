9003b6818eac2afeb8d9f7c411b40e805fa8e588
yunimaxxik <nicxefonn@yandex.ru>
2026-06-21 20:56:56 +0300

    fixed bags

--- index.js
+++ index.js
@@ -1,103 +1,18 @@
 const express = require('express');
-const indexRouter = require('./routes/index');
-
-app.use('/', indexRouter);
-
-const stor = {
-  books: []
-};
+const booksRouter = require('./routes/books');
+const userRouter = require('./routes/user');
+const error404 = require('./middleware/err-404');
 
 const app = express();
+
 app.use(express.json());
 
-app.post('/api/user/login', (req, res) => {
-  res.status(201).json({ id: 1, mail: 'test@mail.ru' });
-});
+app.use('/api/books', booksRouter);
+app.use('/api/user', userRouter);
 
-app.get('/api/books', (req, res) => {
-  const { books } = stor;
-  res.json(books);
-});
-
-app.get('/api/books/:id', (req, res) => {
-  const { books } = stor;
-  const { id } = req.params;
-  const idx = books.findIndex((el) => el.id === id);
-
-  if (idx !== -1) {
-    res.json(books[idx]);
-  } else {
-    res.status(404).json('404 | страница не найдена');
-  }
-});
-
-app.post('/api/books', (req, res) => {
-  const { books } = stor;
-  const {
-    title,
-    description,
-    authors,
-    favorite,
-    fileCover,
-    fileName,
-    fileBook
-  } = req.body;
-  const newBook = new Book(
-    title,
-    description,
-    authors,
-    favorite,
-    fileCover,
-    fileName,
-    fileBook
-  );
-  books.push(newBook);
-  res.status(201).json(newBook);
-});
-
-app.put('/api/books/:id', (req, res) => {
-  const { books } = stor;
-  const { id } = req.params;
-  const {
-    title,
-    description,
-    authors,
-    favorite,
-    fileCover,
-    fileName,
-    fileBook
-  } = req.body;
-  const idx = books.findIndex((el) => el.id === id);
-
-  if (idx !== -1) {
-    books[idx] = {
-      ...books[idx],
-      title,
-      description,
-      authors,
-      favorite,
-      fileCover,
-      fileName,
-      fileBook
-    };
-
-    res.json(books[idx]);
-  } else {
-    res.status(404).json('404 | страница не найдена');
-  }
-});
-
-app.delete('/api/books/:id', (req, res) => {
-  const { books } = stor;
-  const { id } = req.params;
-  const idx = books.findIndex((el) => el.id === id);
-  if (idx !== -1) {
-    books.splice(idx, 1);
-    res.json('ok');
-  } else {
-    res.status(404).json('404 | страница не найдена');
-  }
-});
+app.use(error404);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
